f818a0bf344f6bb39589e77bee02817126ae8943
Nguyen Ngoc Thuong <nguyen.ngoc.thuong@sun-asterisk.com>
2021-08-16 00:42:16 +0700

    update deploy file

--- deploy.php
+++ deploy.php
@@ -25,6 +25,7 @@ host('3.139.84.92')
     ->forwardAgent(false);
 
 task('build:fast', function () {
+    run('cd {{deploy_path}} && git reset --hard');
     run('cd {{deploy_path}} && git pull origin master');
     run('cd {{deploy_path}} && php artisan config:clear; php artisan migrate; composer dump-autoload; npm run dev');
 });
