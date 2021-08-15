2ae1106dc0f15944c55075bf2baa447f446060b3
Nguyen Ngoc Thuong <nguyen.ngoc.thuong@sun-asterisk.com>
2021-08-15 09:15:33 +0700

    add footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,17 @@
-<footer class="blog-footer">
-  <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-  <p>
-    <a href="#">Back to top</a>
-  </p>
-</footer>
+<div class="footer-basic">
+  <footer>
+    <div class="social">
+      <a href="https://www.facebook.com/thuongnguyen130499"><i class="bi bi-facebook"></i></a>
+      <a href="#"><i class="bi bi-instagram"></i></a>
+      <a href="#"><i class="bi bi-twitter"></i></a>
+    </div>
+    <ul class="list-inline">
+      <li class="list-inline-item"><a href="https://nguyenthuong1304.github.io/mysefl/">Profile me</a></li>
+      <li class="list-inline-item"><a href="#">Services</a></li>
+      <li class="list-inline-item"><a href="#">About</a></li>
+      <li class="list-inline-item"><a href="#">Terms</a></li>
+      <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+    </ul>
+    <p class="copyright">Nguyen Thuong</p>
+  </footer>
+</div>
