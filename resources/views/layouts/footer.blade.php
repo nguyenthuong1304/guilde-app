5af7183246cd46803f51de44f69b9a10271dea1d
Nguyen Ngoc Thuong <nguyen.ngoc.thuong@sun-asterisk.com>
2021-08-15 09:21:45 +0700

    add target

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,13 @@
 <div class="footer-basic">
   <footer>
     <div class="social">
-      <a href="https://www.facebook.com/thuongnguyen130499"><i class="bi bi-facebook"></i></a>
+      <a target="_blank" href="https://www.facebook.com/thuongnguyen130499"><i class="bi bi-facebook"></i></a>
       <a href="#"><i class="bi bi-instagram"></i></a>
       <a href="#"><i class="bi bi-twitter"></i></a>
+      <a target="_blank" href="https://nguyenthuong1304.github.io/mysefl/"><i class="bi bi-link"></i></a>
     </div>
     <ul class="list-inline">
-      <li class="list-inline-item"><a href="https://nguyenthuong1304.github.io/mysefl/">Profile me</a></li>
+      <li class="list-inline-item"><a target="_blank" href="https://nguyenthuong1304.github.io/mysefl/">Profile me</a></li>
       <li class="list-inline-item"><a href="#">Services</a></li>
       <li class="list-inline-item"><a href="#">About</a></li>
       <li class="list-inline-item"><a href="#">Terms</a></li>
