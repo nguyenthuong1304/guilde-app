ce2b0964f42fe2e070a7d732dc7f063f8c1f2a36
Nguyen Ngoc Thuong <nguyen.ngoc.thuong@sun-asterisk.com>
2021-08-20 18:12:04 +0700

    remove js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,6 @@
   </main>
   @include('layouts.footer')
   <script src="{{ asset('js/app.js') }}" defer></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   @livewireScripts
   @yield('scripts')
   @stack('scripts')
