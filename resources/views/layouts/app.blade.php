1f6e62f8a10ca2d9bfbab7852c0516d38f242c9b
Nguyen Ngoc Thuong <nguyen.ngoc.thuong@sun-asterisk.com>
2021-08-20 18:05:47 +0700

    remove css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,8 +22,7 @@
   </main>
   @include('layouts.footer')
   <script src="{{ asset('js/app.js') }}" defer></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   @livewireScripts
   @yield('scripts')
   @stack('scripts')
