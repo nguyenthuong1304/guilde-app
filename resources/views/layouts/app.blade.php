62618c46c0ceaae7ac93d94c3a18f406af9e9aab
Nguyen Ngoc Thuong <nguyen.ngoc.thuong@sun-asterisk.com>
2021-08-20 18:03:09 +0700

    remove js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,12 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
   <link rel="icon" href="/favicon.ico" type="image/x-icon">
-  <title>GUIDE</title>
-  <script src="{{ asset('js/app.js') }}" defer></script>
+  <title>Chia sẻ lập trình</title>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
   @livewireStyles
@@ -23,9 +21,9 @@
     @yield('main')
   </main>
   @include('layouts.footer')
+  <script src="{{ asset('js/app.js') }}" defer></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="{{ asset('js/app.js') }}" defer></script>
   @livewireScripts
   @yield('scripts')
   @stack('scripts')
