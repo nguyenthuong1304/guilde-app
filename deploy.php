b5fe0dcc92820f330eb08f97b6735c8db63a5de7
Nguyen Ngoc Thuong <nguyen.ngoc.thuong@sun-asterisk.com>
2021-09-04 16:25:59 +0700

    update clear view

--- deploy.php
+++ deploy.php
@@ -27,7 +27,9 @@ host('3.139.84.92')
 task('build:fast', function () {
     run('cd {{deploy_path}} && git pull origin master');
     // run('cd {{deploy_path}} && composer install');
+    run('cd {{deploy_path}} && php artisan view:clear');
     run('cd {{deploy_path}} && php artisan config:clear; php artisan migrate; composer dump-autoload; npm run dev');
+    run('cd {{deploy_path}} && php artisan view:cache');
 });
 
 after('deploy:failed', 'deploy:unlock');
