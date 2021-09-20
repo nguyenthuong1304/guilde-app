9354cf80661669c535d63181760f04583195e6c6
Nguyen Ngoc Thuong <nguyen.ngoc.thuong@sun-asterisk.com>
2021-09-20 18:12:18 +0700

    fix redierct nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,7 +1,7 @@
 <div class="nav-scroller py-1 mb-2">
   <nav class="nav d-flex justify-content-between">
     @foreach($categories as $cate)
-      <div class="dropdown main-cate" data-bs-toggle="dropdown" aria-expanded="false">
+      <div class="dropdown main-cate">
         <a class="p-2 dropdown-item link-secondary" href="{{ route('category', $cate) }}"> {{ $cate->name}} </a>
         @if($cate->children->count())
         <ul class="dropdown-menu dropdown-menu-center">
