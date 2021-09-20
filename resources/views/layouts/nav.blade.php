7d74fc69d5baf16107931f093c26014fe3aa6e83
Nguyen Ngoc Thuong <nguyen.ngoc.thuong@sun-asterisk.com>
2021-09-20 17:51:06 +0700

    fix part 2

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,14 +1,16 @@
 <div class="nav-scroller py-1 mb-2">
   <nav class="nav d-flex justify-content-between">
     @foreach($categories as $cate)
-      <div class="dropdown main-cate">
+      <div class="dropdown main-cate" data-bs-toggle="dropdown" aria-expanded="false">
         <a class="p-2 dropdown-item link-secondary" href="{{ route('category', $cate) }}"> {{ $cate->name}} </a>
         @if($cate->children->count())
-        <div class="position-relative dropdown-menu">
+        <ul class="dropdown-menu">
           @foreach($cate->children as $cateC)
-            <a class="p-2 dropdown-item link-secondary" href="{{ route('category', $cateC) }}"> {{ $cateC->name}} </a>
+            <li>
+              <a class="p-2 dropdown-item link-secondary" href="{{ route('category', $cateC) }}"> {{ $cateC->name}} </a>
+            </li>
           @endforeach
-        </div>
+        </ul>
         @endif
       </div>
     @endforeach
